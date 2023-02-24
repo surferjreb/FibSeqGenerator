4dc373f917fec095bfdcfc486510a3a12d64cb78
[surferjreb] <[surferjreb@live.com]>
2023-02-24 17:43:41 -0500

    fun...

---
+++ com/brownj/FibSeq.java
@@ -0,0 +1,61 @@
+package com.brownj;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class FibSeq {
+    
+    private int sequenceLength = 0;
+    private List<Integer> fib;
+
+    public FibSeq(int n){
+        this.sequenceLength = n;
+        this.fib = new ArrayList<>(n);
+    }
+
+    private void generateFibSeq(){
+        int a = 0;
+        int b = 0;
+
+        for(int i =0; i < this.sequenceLength; i++){
+            
+            if(i > 1){
+                a = fib.get(i-2);
+                b = fib.get(i-1) + a;
+                fib.add(b);
+
+            } else {
+                fib.add(i);
+            }
+        }
+
+    }
+
+    private void printFibSequence(){
+        for(int i=0; i < fib.size(); i++){
+            String seq = String.format("%d: %d", i, fib.get(i));
+            System.out.print(seq);
+        }
+    }
+
+    protected List<Integer> getFibSeq(){
+        if(!fib.isEmpty()){
+            return fib;
+        }
+
+        return null;
+    }
+
+    public void showFib(){
+        this.generateFibSeq();
+        this.printFibSequence();
+    }
+
+    @Override
+    public String toString(){
+        
+        return String.format("the sequence length is: %d", this.sequenceLength);
+    }
+}
---
+++ com/brownj/Main.java
@@ -0,0 +1,35 @@
+package com.brownj;
+
+import java.util.Scanner;
+
+class Main {
+    
+    public static void main(String[] args){
+        System.out.println("#######################################");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the length of fibonacci Sequence you want: ");
+        String a = input.nextLine();
+        try{
+            int num = Integer.parseInt(a);
+            FibSeq fibSeq = new FibSeq(num);
+            fibSeq.showFib();
+        } catch(NumberFormatException nfe) {
+            System.out.println("The entry you provided is not a whole number, please try again...");
+            System.out.println(nfe.getMessage());
+            try{
+                input.reset();
+                System.out.print("Enter the length of fibonacci Sequence you want: ");
+                a = input.nextLine();
+                int num = Integer.parseInt(a);
+                FibSeq fibSeq = new FibSeq(num);
+                fibSeq.showFib();
+            } catch(NumberFormatException nf) {
+                System.out.println("You provided an entry again that is not a whole number, Have a good day...");
+                System.out.println(nf.getMessage());
+            }
+        } finally{
+            input.close();
+        }
+    }
+
+}
